1e488aab71516eb1341e8576a1df2ffafcec3d9b
Bahadir Balban <balban@getbuzz.io>
2020-03-12 22:02:07 -0700

    Full react component shadowing example

--- src/gatsby-theme-apollo-core/components/logo.js
+++ src/gatsby-theme-apollo-core/components/logo.js
@@ -1,2 +1,38 @@
 import React from 'react';
-export {ReactComponent as default} from '../../assets/saasbox-logo-round.jpg';
+import {ReactComponent as GraphQL} from '../../assets/saasbox-logo-round.jpg';
+import {graphql, useStaticQuery} from 'gatsby';
+import styled from '@emotion/styled';
+import {colors} from 'gatsby-theme-apollo-core';
+
+const Wrapper = styled.div({
+  display: 'flex',
+  alignItems: 'center'
+});
+
+const Title = styled.span({
+  fontSize: 24,
+  marginLeft: 16
+});
+
+export default function Logo() {
+  const data = useStaticQuery(
+    graphql`
+      {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  );
+
+  return (
+    <Wrapper>
+      <GraphQL width={48} height={48} fill={colors.primary} />
+      <Title style={{color: colors.secondary}}>
+        {data.site.siteMetadata.title}
+      </Title>
+    </Wrapper>
+  );
+}
